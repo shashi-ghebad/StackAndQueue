778b762b821b56cda9dc13c54dd5a3453f03ff13
Shashi <shashihghebad3@gmail.com>
2021-03-27 11:13:49 +0530

    [SHASHI]Add:UC1-StackCreate

---
+++ StackAndQueueNew/LinkedListStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    class LinkedListStack
+    {
+        private Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+
+        //30
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;//1300
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;//1400
+            }
+        }
+
+    }
+
+}
---
+++ StackAndQueueNew/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackAndQueue;
+
+namespace StackAndQueueNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            //linkedListStack.Peek(); top.data
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            Console.ReadKey();
+        }
+
+    }
+}
